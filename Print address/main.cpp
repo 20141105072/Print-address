c53649fe0f35130fd0aa5f31737d672e4dc85321
20141105072 <2196690899@qq.com>
2015-12-09 19:55:36 +0800

    print address
    地址打印。定义宏变量N＝5，将变量a的地址赋值给指针变量p，最后以十六进制输出地址。

--- Print address/main.cpp
+++ Print address/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
-
+#define N 5
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+     // insert code here...
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x  &a=%x  &a=%x  p=%x  *p=%x\n",a,&a,&p,p,*p);
     return 0;
 }
+
